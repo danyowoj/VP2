1100877a3704d66647e907ab5dab86fe6b8bce2b
danyowoj <latsuk1803@gmail.com>
2024-11-09 19:09:14 +0700

    Add color pallete

---
+++ Lab_1/Lab_1/shapecalculator.cpp
@@ -0,0 +1,6 @@
+#include "shapecalculator.h"
+
+ShapeCalculator::ShapeCalculator()
+{
+
+}
---
+++ Lab_1/Lab_1/shapecalculator.h
@@ -0,0 +1,11 @@
+#ifndef SHAPECALCULATOR_H
+#define SHAPECALCULATOR_H
+
+
+class ShapeCalculator
+{
+public:
+    ShapeCalculator();
+};
+
+#endif // SHAPECALCULATOR_H
